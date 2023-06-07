655f722f6c153ec457726bcc36c2323daea14fc9
md-masum <masum.nextu@gmail.com>
2023-06-08 00:55:16 +0600

    responsive carosel added

---
+++ src/Pages/Home/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import img1 from '../../../../assets/english.jpg'
+import img2 from '../../../../assets/japanese.jpg'
+import img3 from '../../../../assets/rassan.jpg'
+
+const Banner = () => {
+    return (
+        <div className='h-60'>
+             <Carousel>
+                <div>
+                    <img src={img1} />
+                </div>
+                <div>
+                    <img src={img2} />
+                </div>
+                <div>
+                    <img src={img3} />
+                </div>
+            </Carousel>
+        </div>
+    );
+};
+
+export default Banner;
