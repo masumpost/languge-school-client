70da9387c0689e386ac246fc4e105fea6b5301be
md-masum <masum.nextu@gmail.com>
2023-06-11 10:52:27 +0600

    class add on classes page

--- src/Pages/Classes/Classes.jsx
+++ src/Pages/Classes/Classes.jsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+import ClassCard from "../../components/ClassCard";
 
 const Classes = () => {
+
+    const [classes, setClasses] = useState([]);
+
+    useEffect(()=> {
+        fetch('classes.json')
+        .then(res => res.json())
+        .then(data =>setClasses(data))
+    },[])
     return (
-        <div>
-            Here is show all Classes
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-8 p-4">
+           {
+            classes.map(item => <ClassCard
+                key={item.id}
+                item={item}
+                ></ClassCard>)
+           }
         </div>
     );
 };
