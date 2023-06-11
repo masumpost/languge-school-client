36b8561130ce72dc7f1aef43ae156afccf972491
md-masum <masum.nextu@gmail.com>
2023-06-11 11:32:26 +0600

    helmet added on instructors page

--- src/Pages/Instructros/Instructors.jsx
+++ src/Pages/Instructros/Instructors.jsx
@@ -1,23 +1,29 @@
 import { useEffect, useState } from "react";
 import TeachersCard from "../../components/TeachersCard";
+import { Helmet } from "react-helmet-async";
 
 const Instructors = () => {
     const [teachers, setTeachers] = useState([]);
 
-    useEffect(()=> {
+    useEffect(() => {
         fetch('teachers.json')
-        .then(res => res.json())
-        .then(data =>setTeachers(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setTeachers(data));
+    }, [])
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 my-8">
-            {
-                teachers.map(teacher => <TeachersCard
-                key={teacher.id}
-                teacher={teacher}
-                ></TeachersCard>)
-            }
-        </div>
+        <>
+            <Helmet>
+                <title>Instructors - L School</title>
+            </Helmet>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 my-8">
+                {
+                    teachers.map(teacher => <TeachersCard
+                        key={teacher.id}
+                        teacher={teacher}
+                    ></TeachersCard>)
+                }
+            </div>
+        </>
     );
 };
 
